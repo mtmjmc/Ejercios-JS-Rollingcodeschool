355922ca248811728e03f935fbcb1979baad15bd
mtmjmc <juanmcruz997@gmail.com>
2023-11-01 14:04:38 -0300

    segunda parte

---
+++ js/Ejercicio-5.js
@@ -0,0 +1,3 @@
+var n1 = prompt("Escribir un número");
+var n2 = prompt("Escribir otro numero número");
+document.write("El resultado es: "+ ( parseInt(n1)+ parseInt(n2) ) ) ;
---
+++ js/Ejercicio-7.js
@@ -0,0 +1,13 @@
+var n1 = parseInt (prompt("Escribir un número"));
+var n2 = parseInt (prompt("Escribir segundo número"));
+var n3 = parseInt (prompt("Escribir tercer número"));
+if (n1 > n2 && n1 > n3) {
+    document.write("El "+(n1)+ " es el número más grande" );
+    } 
+    else if (n2 > n3)
+     {
+        document.write("El "+(n2)+ " es el número más grande" );
+    } else {
+        document.write("El "+(n3)+ " es el número más grande" );
+    }
+    
